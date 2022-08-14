e44da4d0d9aed975e2aaeaa9889b1aa175e735b7
Tatiana <tatum@rambler.ru>
2022-08-14 23:01:46 +0300

    Task 023

---
+++ Task023/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход число (N)
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+void CubeNumbers(int N)
+{
+    int i = 1;
+    while(i < N)
+    {
+        System.Console.Write($"{Math.Pow(i, 3)}, ");
+        i++;
+    }
+    System.Console.WriteLine($"{Math.Pow(N, 3)}");
+}
+
+System.Console.WriteLine("Введите положительное число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N >= 0)
+{
+    CubeNumbers(N);
+}
+else System.Console.WriteLine("Вы ввели отрицательное число");
+
+
