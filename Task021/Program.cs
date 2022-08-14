45d0682aefef32ae61ed9d6c3ec1ae367793b49d
Tatiana <tatum@rambler.ru>
2022-08-14 21:54:43 +0300

    Task 021

---
+++ Task021/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход координаты 
+//двух точек и находит расстояние между ними в 3D пространстве.
+double Distance(double[] A, double[] B)
+{
+    double distance = Math.Sqrt(Math.Pow(A[0] - B[0], 2) + Math.Pow(A[1] - B[1], 2) + Math.Pow(A[2] - B[2], 2));
+    Console.WriteLine(Math.Round(distance, 2));
+    return distance;
+}
+
+Distance(new double[] {3, 6, 8}, new double[] {2, 1, -7});
+Distance(new double[] {7, -5, 0}, new double[] {1, -1, 9});
